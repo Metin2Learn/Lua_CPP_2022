29764a34609d89b3f5d18413533566d840f469e6
liukefu <liukefu2050@sina.com>
2025-01-02 14:33:08 +0800

    调试

--- 7_C++Lua_lib/main.cpp
+++ 7_C++Lua_lib/main.cpp
@@ -5,19 +5,20 @@
 #include <string>
 #include "lua.hpp"
 #include "../config/env_config.h"
-
+#include <iostream>
 int main(){
+    system("CHCP 65001");
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
     printf("\n");
     printf("=============== Lua调用 ===============\n");
-    std::string fname = PROJECT_PATH +"/7、Lua调用C++的lib/Lua加载C模块.lua";
+    std::string fname = PROJECT_PATH +"/7_C++Lua_lib/LuaC.lua";
     if (luaL_loadfile(L, fname.c_str()) || lua_pcall(L, 0, 0, 0)) {
         printf("can't run config. file: %s", lua_tostring(L, -1));
     }
 
     lua_close(L);
-
+    std::cin.get();
     return 1;
 }
